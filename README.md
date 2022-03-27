f995d0d7cd19bb37d1fc74626221957cf01fccf2
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-03-27 23:53:54 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My website
+
+Visit [My website](https://damin.dev).
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly"> Hello, </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
